0a6a384092997139ee7b44f21bb28af0fd0bfedf
Mario Parra de Miguel <mparrade@redhat.com>
2023-06-13 14:48:04 +0200

    first commit

--- code/app.js
+++ code/app.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const fs = require('fs');
+const filePath = '/tmp/message';
 const server = http.createServer((req, res) => {
   let message = process.env.MESSAGE || "Hello world! - Default";
+  if (fs.existsSync(filePath)) {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        message = data.trim();
+      } catch (err) {
+        console.error('Error reading the file:', err);
+      }
+  }
   console.log("requested.");
-  message = process.env.MESSAGE || "Hello world! - Default";
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(message);
